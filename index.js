837f88f6a49c19c72a31acec94bd123a85c89d3d
minh_vu <leminhvu0201@gmail.com>
2024-03-11 17:40:53 +0700

    mv

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override');
 const flash = require('express-flash');
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const MemoryStore = require('memorystore')(session);
 const database = require("./config/database.js");
 const route = require("./routes/client/index.route");
 const bodyParser = require('body-parser');
@@ -27,9 +28,12 @@ app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 app.use(cookieParser('333'));
 app.use(session({
-    secret: "123",
-    resave: true,
-    saveUninitialized: true
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    resave: false,
+    secret: 'keyboard cat'
 }));
 app.use(flash());
 app.use(express.static(`${__dirname}/public`));
