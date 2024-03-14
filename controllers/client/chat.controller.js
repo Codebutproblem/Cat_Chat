b485a195884a0ef53bdb079ac21bddd654fbb20a
minh_vu <leminhvu0201@gmail.com>
2024-03-15 00:11:01 +0700

    push mv

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -7,7 +7,6 @@ const chatSocket = require("../../socket/client/chat.socket");
 const Pair = require("../../helpers/pair");
 const pairSocket = require("../../socket/client/pair.socket");
 module.exports.index = async (req, res) => {
-    await Chat.deleteMany({});
     const room_id = req.params.room_id;
     const idA = res.locals.user.id;
     chatSocket(res, room_id, "friend");
